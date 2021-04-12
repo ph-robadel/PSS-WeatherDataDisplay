44c4d626a706f205081bf2d4480c2ffef3c25c29
Pedro Henrique Robadel <ph.robadel@gmail.com>
2021-04-11 22:02:39 -0300

    Criação da estrutura base do projeto

---
+++ WeatherDataDisplay/src/main/java/br/ufes/weatherdatadisplay/principal/Principal.java
@@ -0,0 +1,11 @@
+package br.ufes.weatherdatadisplay.principal;
+
+/**
+ *
+ * @author Pedro Henrique Robadel
+ */
+public class Principal {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
