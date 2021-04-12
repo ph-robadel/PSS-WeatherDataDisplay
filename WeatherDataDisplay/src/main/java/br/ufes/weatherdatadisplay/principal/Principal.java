d5ae50cd8070b4d524947a3d3b7d3689acf69152
Pedro Henrique Robadel <ph.robadel@gmail.com>
2021-04-12 11:16:58 -0300

    Conclusão do pacote View

--- WeatherDataDisplay/src/main/java/br/ufes/weatherdatadisplay/principal/Principal.java
+++ WeatherDataDisplay/src/main/java/br/ufes/weatherdatadisplay/principal/Principal.java
@@ -1,11 +1,13 @@
 package br.ufes.weatherdatadisplay.principal;
 
+import br.ufes.weatherdatadisplay.view.PrincipalView;
+
 /**
  *
  * @author Pedro Henrique Robadel
  */
 public class Principal {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        PrincipalView principalView = new PrincipalView();
     }
 }
